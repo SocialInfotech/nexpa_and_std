7f916e30f47a2398a0b8d6dc655c4c20e217d6c1
lui <lui.donios@gmail.com>
2016-04-18 16:35:01 +0800

    -removed unused imports

--- src/com/lpoezy/nexpa/activities/CommentsFragment.java
+++ src/com/lpoezy/nexpa/activities/CommentsFragment.java
@@ -18,7 +18,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.bartoszlipinski.recyclerviewheader2.RecyclerViewHeader;
 import com.google.gson.Gson;
 import com.lpoezy.nexpa.R;
 import com.lpoezy.nexpa.chatservice.XMPPService;
@@ -52,7 +51,6 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
